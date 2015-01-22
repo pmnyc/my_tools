7bd0bebd6a72bdc27a9c2120de7b256d69497842
Man <pmiori@gmail.com>
2015-01-22 12:38:14 -0500

    detect python version and OS system

---
+++ whichPythonVersion.R
@@ -0,0 +1,24 @@
+whichPython2use_Fun = function(
+                            cmdpy_alter1 = "python2.7",
+                            cmdpy_alter2 = "python2.6",
+                            pyCommd = "-V"){
+    ## This is to find which python version to run certain packages/codes
+        # in the case when some codes are only compatible with certain version
+        # of Python version
+    # Sample Inputs
+    # cmdpy_alter1 = "python2.7";
+    # cmdpy_alter2 = "python2.6";
+    # pyCommd = ""  #this is to define the command after $ python2.7 or $ python2.6
+
+    cmdpy = cmdpy_alter1;
+
+    msg = tryCatch({system2(command=cmdpy,args = pyCommd)}, warning=function(w){print(w)});
+    if (toString(msg) != "0"){
+        cmdpy = cmdpy_alter2
+    }
+
+    return(cmdpy)
+
+    print(paste(cmdpy,"is the python version to call GDAL"));
+    print(paste("The OS system is",.Platform$OS.type));
+}
