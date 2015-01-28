9f6b764a6862eca197be5871321ee285a7e28e2f
Man <pmiori@gmail.com>
2015-01-27 20:13:03 -0500

    new small tools

--- random_name_generator.py
+++ random_name_generator.py
@@ -3,6 +3,8 @@
 Create a random ID Name of length 12
 @author: pm
 """
+
+
 import string, random
 
 def id_generator(size=12, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
