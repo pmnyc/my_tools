23e467d364063844f2e91b5ed05d478e9ec3d834
Man <pmiori@gmail.com>
2015-01-27 20:07:45 -0500

    new small tools

---
+++ docker_basic_operation.sh
@@ -0,0 +1,6 @@
+echo 'assign random container name' 
+x=random_container_name 
+echo 'create a docker through a bash command script' 
+sudo docker run --name $x pengma/aws_ubuntu:v5 /bin/sh -c 'mkdir ~/image; cd ~/image; echo 1 > test.txt;' 
+echo 'copy containers test.txt file into the container_output folder under host current path'
+sudo docker cp $x:/root/image/test.txt $(pwd)/container_output 
---
+++ random_name_generator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Create a random ID Name of length 12
+@author: pm
+"""
+import string, random
+
+def id_generator(size=12, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+print id_generator(12)
