46d7f6141c9122dd1635c52f4a6602a3d4bfc551
Man <pmiori@gmail.com>
2015-03-19 00:53:45 -0400

    Find files with path pattern match on S3

--- file_pattern_match_on_s3.py
+++ file_pattern_match_on_s3.py
@@ -3,46 +3,49 @@
 @author: pm
 
 This Program searches for the pattern of files in the s3 bucket and
-    returns the files that match the pattern and also the my_id
+    returns the files that match the pattern and also the myid
     associated with the file
 
 Sample Usage:
     > import file_pattern_match_on_s3
-    > fileobj = file_pattern_match_on_s3.main(search_file_path_pattern = "mybucket/result/*/calibration/1000*results.json")
+    > fileobj = file_pattern_match_on_s3.main(search_file_path_pattern = "mybucket/results/*/ca/*results.json", use_s3key_ind=False)
     > print fileobj.filelist
-    > print fileobj.my_id
+    > print fileobj.myid
     > print fileobj.filename
 """
 
 import json, os
 import boto, fnmatch
+from boto.s3.connection import S3Connection
+from boto.s3.connection import OrdinaryCallingFormat
 
 class filePatternMatch(object):
-    def __init__(self, search_file_path_pattern, s3_key_info="s3_key.json", wildcard="*"):
+    def __init__(self, search_file_path_pattern, use_s3key_ind=False, s3_key_info="s3_key.json", wildcard="*"):
         self.search_file_path_pattern = search_file_path_pattern
         self.s3_key_info = s3_key_info
         self.wildcard = wildcard
+        self.use_s3key_ind = use_s3key_ind
     
     def getStructID(self,x,search_file_path_pattern,wildcard):
-        # This function gives my_id masked by wildcard by matching x with search_file_path_pattern, in this example
+        # This function gives myid masked by wildcard by matching x with search_file_path_pattern, in this example
             # the result should be 100000
         ## Sample Input
-        # x = "mybucket/result/100000/calibration/1000_results.json" #this defines the actual file name
-        # search_file_path_pattern = "mybucket/result/*/calibration/1000*results.json" #this is the pattern of file for matching
+        # x = "mybucket/result/100000/ca/1000_results.json" #this defines the actual file name
+        # search_file_path_pattern = "mybucket/result/*/ca/1000*results.json" #this is the pattern of file for matching
         ##
         search_short = search_file_path_pattern[:search_file_path_pattern.find(wildcard)]
         search_short_list = search_short.split("/")
         search_short_list = [i for i in search_short_list if i != '']
         x_split = x.split("/")
         x_split = [i for i in x_split if i != '']
-        my_id = x_split[len(search_short_list)]
-        return my_id
+        myid = x_split[len(search_short_list)]
+        return myid
 
     def matchPatternInd(self,x,search_file_path_pattern,wildcard):
         # This function gives boolean result if string x maches the pattern given by search_file_path_pattern using wildcard
         ## Sample Input
-        # x = "mybucket/result/100000/calibration/1000_results.json" #this defines the actual file name
-        # search_file_path_pattern = "mybucket/result/*/calibration/1000*results.json" #this is the pattern of file for matching
+        # x = "mybucket/result/100000/ca/1000_results.json" #this defines the actual file name
+        # search_file_path_pattern = "mybucket/result/*/ca/1000*results.json" #this is the pattern of file for matching
         ##
         if search_file_path_pattern.count(wildcard) == 0:
             return (x == search_file_path_pattern)
@@ -65,8 +68,8 @@ class filePatternMatch(object):
     def wildcardmatch(self,x,search_file_path_pattern,wildcard):
         # This function gives boolean result if string x maches the pattern given by search_file_path_pattern using wildcard
         ## Sample Input
-        # x = "mybucket/result/100000/calibration/1000_results.json" #this defines the actual file name
-        # search_file_path_pattern = "mybucket/result/*/calibration/1000*results.json" #this is the pattern of file for matching
+        # x = "mybucket/result/100000/ca/1000_results.json" #this defines the actual file name
+        # search_file_path_pattern = "mybucket/result/*/ca/1000*results.json" #this is the pattern of file for matching
         ##
         x = x.replace(wildcard,"*")
         search_file_path_pattern = search_file_path_pattern.replace(wildcard,"*")
@@ -89,45 +92,55 @@ class filePatternMatch(object):
     def createListofFilesOnS3(self):
         search_file_path_pattern = self.search_file_path_pattern
         s3_key_info = self.s3_key_info
+        use_s3key_ind = self.use_s3key_ind
         # s3_bucket can be referred easily from string search_file_path_pattern
         s3_bucket = search_file_path_pattern.split("/")[0]
         if not s3_bucket.lower().startswith("s3://"):
             s3_bucket = "s3://" + s3_bucket
-        json_config = self.loadJson(s3_key_info)
-        AWS_ACCESS_KEY_ID = json_config["AWS_ACCESS_KEY_ID"]
-        AWS_SECRET_ACCESS_KEY = json_config["AWS_SECRET_ACCESS_KEY"]
-        #AWS_DEFAULT_REGION = json_config["AWS_DEFAULT_REGION"]
-        conn = boto.connect_s3(
-            aws_access_key_id = AWS_ACCESS_KEY_ID,
-            aws_secret_access_key = AWS_SECRET_ACCESS_KEY, #is_secure=False # uncomment if you are not using ssl
-            )
-        s3_bucket = self.addSlash(s3_bucket)
-        bucket = conn.get_bucket(s3_bucket.replace("s3://","").split("/")[0])
-        buck = bucket.list()
+        
+        if os.path.exists(os.path.join(os.getcwd(),s3_key_info)) and use_s3key_ind:
+            json_config = self.loadJson(s3_key_info)
+            AWS_ACCESS_KEY_ID = json_config["AWS_ACCESS_KEY_ID"]
+            AWS_SECRET_ACCESS_KEY = json_config["AWS_SECRET_ACCESS_KEY"]
+            #AWS_DEFAULT_REGION = json_config["AWS_DEFAULT_REGION"]
+            conn = boto.connect_s3(
+                aws_access_key_id = AWS_ACCESS_KEY_ID,
+                aws_secret_access_key = AWS_SECRET_ACCESS_KEY #is_secure=False # uncomment if you are not using ssl
+                )
+            s3_bucket = self.addSlash(s3_bucket)
+            bucket = conn.get_bucket(s3_bucket.replace("s3://","").split("/")[0])
+            buck = bucket.list()
+        else:
+            conn = S3Connection(calling_format=OrdinaryCallingFormat())
+            s3_bucket = self.addSlash(s3_bucket)
+            bucket = conn.get_bucket(s3_bucket.replace("s3://","").split("/")[0])
+            buck = bucket.list()
+        
         bucket_name = s3_bucket.replace("s3://","").replace("/","")
         files = [(bucket_name + "/" + key.name) for key in buck]
         if len(files) == 0:
             filelist = None
         else:
             filelist = []
-            my_id = []
+            myid = []
             for i in range(len(files)):
                 if self.wildcardmatch(files[i], search_file_path_pattern, self.wildcard):
                     filelist.append("s3://" + files[i])
                     id = self.getStructID(files[i], search_file_path_pattern, self.wildcard)
-                    my_id.append(id)
+                    myid.append(id)
 
         self.filelist = filelist
-        self.my_id = my_id
-        getfilename = lambda t: os.path.splitext(os.path.basename(t.split('/')[-1]))[0]
-        self.filename = map(getfilename, filelist)
+        self.myid = myid
+        #getfilename = lambda t: os.path.splitext(os.path.basename(t.split('/')[-1]))[0]
+        getfilename = lambda t: "_".join(t.split('/')[-2:])
+        self.filename = [str(myid[i]) + "_" + getfilename(filelist[i]) for i in range(len(filelist))]
 
 if __name__ in ['__main__','file_pattern_match_on_s3']:
-    def main(search_file_path_pattern, s3_key_info="s3_key.json", wildcard="*"):
+    def main(search_file_path_pattern, use_s3key_ind=False, s3_key_info="s3_key.json", wildcard="*"):
         # sample input
-        # search_file_path_pattern = "mybucket/result/*/calibration/1000*results.json"
+        # search_file_path_pattern = "mybucket/result/*/ca/1000*results.json"
             # where search_file_path_pattern defines the pattern for searching certain files in s3 bucket
-        myobj = filePatternMatch(search_file_path_pattern, s3_key_info, wildcard)
+        myobj = filePatternMatch(search_file_path_pattern, use_s3key_ind, s3_key_info, wildcard)
         myobj.createListofFilesOnS3()
-        # what we cared about are myobj.filelist & myobj.my_id & myobj.filename
+        # what we cared about are myobj.filelist & myobj.myid & myobj.filename
         return myobj
