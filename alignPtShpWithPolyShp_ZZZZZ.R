12ffff432c5b9d838c09b24b88caee84736a068c
Man <pmiori@gmail.com>
2015-04-10 02:34:58 -0400

    zzzzzzzzzz

---
+++ alignPtShpWithPolyShp_ZZZZZ.R
@@ -0,0 +1,11 @@
+########################
+### @author: pm
+### It aligns shape file with point shape files based on elevations
+########################
+
+alignPtShpWithPolyShp <- function(pointshape, polyshape,
+                                polyshape_polygon_area_fieldname="SHAPE_AREA",
+                                pointshape_elev_fieldname="z",
+                                buffer_distance=5){
+                                print 'haha'
+}
