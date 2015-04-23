e3f0b4d2ef0c31a0855170fdcf7c731201d75abf
Man <pmiori@gmail.com>
2015-04-23 01:14:41 -0400

    Docker Basic Commands

--- docker_basic_operation.sh
+++ docker_basic_operation.sh
@@ -3,4 +3,4 @@ x=random_container_name
 echo 'create a docker through a bash command script' 
 sudo docker run --name $x pengma/aws_ubuntu:v5 /bin/sh -c 'mkdir ~/image; cd ~/image; echo 1 > test.txt;' 
 echo 'copy containers test.txt file into the container_output folder under host current path'
-sudo docker cp $x:/root/image/test.txt $(pwd)/container_output 
+sudo docker cp $x:/root/image/test.txt $(pwd)/container_output
