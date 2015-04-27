9862c260bd2bf93c2a999a7857c7358861e0c985
Man <pmiori@gmail.com>
2015-04-27 13:09:43 -0400

    S3 Bucket Creation and Deletion

--- s3_buckets_creation_deletion.py
+++ s3_buckets_creation_deletion.py
@@ -10,6 +10,9 @@ temp_s3_buckets_bystructid.deleteTempBucket(myid, s3_key_info)
 import boto, json
 import platform
 import subprocess
+from boto.s3.connection import S3Connection
+from boto.s3.connection import OrdinaryCallingFormat
+from fnmatch import fnmatch
 
 def loadJson(filename):
     # Sample input
@@ -80,6 +83,72 @@ def awsCLCopycmd(from_search_pattern, myid, to_bucket, includefile_pattern_overr
     cmd += includeformat
     return cmd
 
+def deleteBucket(bucketname, s3_key_info):
+    json_config = loadJson(s3_key_info)
+    AWS_ACCESS_KEY_ID = json_config["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = json_config["AWS_SECRET_ACCESS_KEY"]
+    conn = boto.connect_s3(
+            aws_access_key_id = AWS_ACCESS_KEY_ID,
+            aws_secret_access_key = AWS_SECRET_ACCESS_KEY) #is_secure=False # uncomment if you are not using ssl
+    full_bucket = conn.get_bucket(bucketname)
+    for key in full_bucket.list():
+        key.delete()
+    conn.delete_bucket(bucketname)
+    del conn
+
+def copyS3Files(s3_key_info, from_search_pattern, struct_id, to_bucket, includefile_pattern_override=None,
+                file_include = ["*results.json","*eplustbl.html"]):    
+    from_bucket = from_search_pattern.split("/")[0]
+    from_file = from_search_pattern.replace(from_bucket+"/", "")
+    from_folder = "/".join(from_file.replace("*",str(struct_id)).split("/")[:-1])
+    
+    json_config = loadJson(s3_key_info)
+    AWS_ACCESS_KEY_ID = json_config["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = json_config["AWS_SECRET_ACCESS_KEY"]
+    conn = S3Connection(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,calling_format=OrdinaryCallingFormat())
+    if to_bucket not in map(lambda t: t.name, conn.get_all_buckets()):
+        createBucket(to_bucket, s3_key_info=s3_key_info)
+    from_bucket_conn = conn.get_bucket(from_bucket)
+    to_bucket_conn = conn.get_bucket(to_bucket)
+    from_folderkey = from_bucket_conn.list(from_folder)
+    #for fromkey in from_folderkey:
+    #    print fromkey.name
+    def matches(x, file_include):
+        count = 0
+        for i in range(len(file_include)):
+            if fnmatch(x,file_include[i]):
+                count += 1
+        if count == 0:
+            return False
+        else:
+            return True
+    fromkey_2 = filter(lambda t: matches(t.name, file_include), from_folderkey)
+    for k in fromkey_2:
+        to_bucket_conn.copy_key(k.key, from_bucket, k.key)
+
+def uploadFile2s3(fileup, s3_output, s3_key_info):
+    # sample input
+    #s3_output = 's3://ngrid.manpeng/coffee/financial.output.temp/'
+    #fileup = 'zzzzz.py'
+    #####
+    def addSlash(x):
+        if not(x.endswith("/")):
+            x += "/"
+        return x
+
+    json_config = loadJson(s3_key_info)
+    AWS_ACCESS_KEY_ID = json_config["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = json_config["AWS_SECRET_ACCESS_KEY"]
+    #AWS_DEFAULT_REGION = json_config["AWS_DEFAULT_REGION"]
+    conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, calling_format=OrdinaryCallingFormat())
+    bucket = s3_output.replace("s3://","").replace("S3://","").split("/")[0]
+    s3_output_folder = addSlash(s3_output).replace("s3://","").replace("S3://","").replace(bucket + "/","").replace(bucket,"")
+    bucket = conn.get_bucket(bucket)
+    
+    newkey = bucket.new_key(s3_output_folder + os.path.basename(fileup))
+    newkey.set_contents_from_filename(fileup)
+
+"""
 def copyS3Files(s3_key_info, from_search_pattern, myid, to_bucket, includefile_pattern_override=None):
     cmd = awsEnvironcmd(s3_key_info=s3_key_info) + awsCLCopycmd(from_search_pattern=from_search_pattern, myid=myid, to_bucket=to_bucket,includefile_pattern_override=includefile_pattern_override)
     x = subprocess.call(cmd, shell=True)
@@ -93,6 +162,7 @@ def deleteBucket(bucketname, s3_key_info):
         print "Bucket " + bucketname + " does not exist. No need to worry. :-)"
     del x
     deleteBucket0(bucketname, s3_key_info=s3_key_info)
+"""
 
 if __name__ in ['__main__', 'temp_s3_buckets_bystructid']:
     def copys3Files2temp(myid, search_pattern, s3_key_info, temp_bucket_suffix = "zzzzzz.", includefile_pattern_override=None):
