ad8d5a2cad15189c50be3166f464b392df60e22b
Man <pmiori@gmail.com>
2015-06-15 22:13:11 -0400

    Save Python Workspace or Restore it

--- workspace_save_restore.py
+++ workspace_save_restore.py
@@ -3,31 +3,38 @@ This program is to save and restore current workspace
     information when using Python IDE
 """
 
-import shelve
+import shelve, os
+import numpy as np
 
 #####################
 # To Save Workspace:
 #####################
-def saveWorkspace(filename):
-    my_shelf = shelve.open(filename,'n') # 'n' for new
-
-    for key in dir():
+def saveWorkspace(f):
+    try:
+        os.remove(f)
+    except:
+        pass
+    my_shelf = shelve.open(f,flag='n') # n means new file
+    dirs = dir()
+    dirs = np.setdiff1d(globals().keys(),dirs)
+    for key in dirs:
         try:
             my_shelf[key] = globals()[key]
         except TypeError:
-            #
             # __builtins__, my_shelf, and imported modules can not be shelved.
-            #
             print('ERROR shelving: {0}'.format(key))
     my_shelf.close()
 
 #####################
 # To restore:
 #####################
-def restoreWorkspace(filename):
-    my_shelf = shelve.open(filename)
+def restoreWorkspace(f):
+    my_shelf = shelve.open(f,flag='r')
     for key in my_shelf:
-        globals()[key]=my_shelf[key]
+        try:
+            globals()[key]=my_shelf[key]
+        except Exception as e:
+            print e
     my_shelf.close()
 
 if __name__ == '__main__':
