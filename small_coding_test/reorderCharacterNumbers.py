7000fb08e7727fc53827273e8fd8e19fc38c7c44
pm <pmiori@gmail.com>
2015-10-10 15:23:43 -0400

    add some test

--- small_coding_test/reorderCharacterNumbers.py
+++ small_coding_test/reorderCharacterNumbers.py
@@ -8,31 +8,31 @@ Lessons:
 2) string.sort() replaces string directly, a=b.sort() IS INVALID !!!
 3) list(string) is to separate the strings into list for better manipulation.
 """
-
-def orderXbytype(x):
-    letters_idx = filter(lambda i: x[i].isalpha(), range(len(x)))
-    num_idx = filter(lambda i: x[i].isdigit(), range(len(x)))
-    space_idx = filter(lambda i: x[i].isspace(), range(len(x)))
-    
-    def sortbyidx(x, idx):
-        x2=map(lambda i: x[i], idx)
-        x2.sort()
-        return x2
-    
-    letters_ = sortbyidx(x, letters_idx)
-    numbers_ = sortbyidx(x, num_idx)
-    
-    x_new = [' '] * len(x)
-    
-    for i in range(len(letters_)):
-        x_new[letters_idx[i]] = letters_[i]
-    for i in range(len(numbers_)):
-        x_new[num_idx[i]] = numbers_[i]
-    return "".join(x_new)
+class Solution:
+    def orderXbytype(x):
+        letters_idx = filter(lambda i: x[i].isalpha(), range(len(x)))
+        num_idx = filter(lambda i: x[i].isdigit(), range(len(x)))
+        space_idx = filter(lambda i: x[i].isspace(), range(len(x)))
+        
+        def sortbyidx(x, idx):
+            x2=map(lambda i: x[i], idx)
+            x2.sort()
+            return x2
+        
+        letters_ = sortbyidx(x, letters_idx)
+        numbers_ = sortbyidx(x, num_idx)
+        
+        x_new = [' '] * len(x)
+        
+        for i in range(len(letters_)):
+            x_new[letters_idx[i]] = letters_[i]
+        for i in range(len(numbers_)):
+            x_new[num_idx[i]] = numbers_[i]
+        return "".join(x_new)
 
 
 if __name__ == '__main__':
     import sys
     stdinput = sys.argv[1]
     print "typed in "+stdinput
-    print "Ouptut is "+orderXbytype(stdinput)
+    print "Ouptut is "+Solution().orderXbytype(stdinput)
